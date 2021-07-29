16309d54f27b0cd5176de78e85cae56029d1f502
Oleksandr Nikitenko <object.020@gmail.com>
2021-07-29 21:41:46 +0300

    Add homework lessons 1

---
+++ Lesson_1/homework.py
@@ -0,0 +1,24 @@
+# TASK 2
+
+# 'My first string'
+print('My first string')
+
+# 'My second string!!!'
+print('My second string', end='!!!\n')
+
+# 'My-third-string'
+print('My', 'third', 'string', sep='-')
+
+
+# TASK 3
+
+# Print 'O' v.1
+print("#########\n#\t\t#\n#\t\t#\n#\t\t#\n#########\n")
+# Print 'O' v.2
+print("#####\n#\t#\n#\t#\n#\t#\n#####\n")
+
+
+# Print 'H' v.1
+print("#\t\t#\n#\t\t#\n#########\n#\t\t#\n#\t\t#\n")
+# Print 'H' v.2
+print("#\t#\n#\t#\n#####\n#\t#\n#\t#\n")
